12d50cac921b7eaed67170fa906c9ded43a5074d
Danilofranc <danilofranco2011@hotmail.com>
2024-01-11 15:13:36 -0300

    Adicionado aba Destination e animações

--- src/components/Crew.js
+++ src/components/Crew.js
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
 import ShuttleworthImage from '../img/image-mark-shuttleworth.png';
 import CrewImage from '../img/image-douglas-hurley.png';
+import VictorGlover from '../img/image-victor-glover.png';
+import AnoushehAnsari from '../img/image-anousheh-ansari.png'
+
 import styles from "../components/modules/Crew.module.css";
 
 export default function Crew() {
@@ -13,31 +16,147 @@ export default function Crew() {
         Douglas Gerald Hurley is an American engineer, former Marine<br />
         Corps pilot and former NASA astronaut. He<br />
         launched into space for the third time as commander of<br />
-        Crew Dragon Demo-2.
+        Crew Dragon Demo-2.<br />
+        <span>&nbsp;</span><br />
       </>
     ),
   };
 
-  const [selectedCrew, setSelectedCrew] = useState(initialCrewState);
+  const [selectedCrew, setSelectedCrew] = useState(initialCrewState); 
 
-  const handleButtonClick = () => {
+  const handleButtonClick1 = () => {
+
+    const button0 = document.getElementById("botao0");
+    const button1 = document.getElementById("botao1");
+    const button2 = document.getElementById("botao2");
+    const button3 = document.getElementById("botao3");
+
+    button0.classList.remove(styles.botaoCrewClick);
+    button0.classList.add(styles.botaoCrew);    
+
+    button1.classList.remove(styles.botaoCrew);
+    button1.classList.add(styles.botaoCrewClick);
+
+    button2.classList.remove(styles.botaoCrewClick);
+    button2.classList.add(styles.botaoCrew);
+
+    button3.classList.remove(styles.botaoCrewClick);
+    button3.classList.add(styles.botaoCrew);
+
+
     setSelectedCrew({
+      
       image: ShuttleworthImage,
       title: "MISSION SPECIALIST",
       name: "MARK SHUTTLEWORTH",
       text: (
         <>
-          Mark Richard Shuttleworth is the founder and CEO of<br />
-          Canonical, the company behind the Linux-based Ubuntu<br />
-          operating system. Shuttleworth became the first South<br />
-          African to travel to space as a space tourist.
+          Mark Richard Shuttleworth is the founder and CEO of<br/>
+          Canonical, the company behind the Linux-based Ubuntu<br/>
+          operating system. Shuttleworth became the first South<br/>
+          African to travel to space as a space tourist.<br />
+          <span>&nbsp;</span><br />          
         </>
       ),
     });
   };
 
-  const handleBackToDouglasClick = () => {
+  const handleButtonClick2 = () => {
+    const button0 = document.getElementById("botao0");
+    const button1 = document.getElementById("botao1");
+    const button2 = document.getElementById("botao2");
+    const button3 = document.getElementById("botao3");
+    
+
+    button0.classList.remove(styles.botaoCrewClick);
+    button0.classList.add(styles.botaoCrew);
+
+    button1.classList.remove(styles.botaoCrewClick);
+    button1.classList.add(styles.botaoCrew);
+
+    button2.classList.remove(styles.botaoCrew);
+    button2.classList.add(styles.botaoCrewClick);
+
+    button3.classList.remove(styles.botaoCrewClick);
+    button3.classList.add(styles.botaoCrew);
+
+    setSelectedCrew({
+        image: VictorGlover,
+        title: "PILOT",
+        name: "VICTOR GLOVER",
+        text: (
+            <>
+                Pilot on the first operational flight of the SpaceX Crew<br/>
+                Dragon to the International Space Station. Glover is a<br/>
+                commander in the U.S. Navy where he pilots an F/A-18.He<br/>
+                was a crew member of Expedition 64, and served as a<br/>
+                station systems flight engineer.
+            </>
+        ),
+    });
+  };
+
+
+
+  const handleButtonClick3 = () => {
+    const button0 = document.getElementById("botao0");
+    const button1 = document.getElementById("botao1");
+    const button2 = document.getElementById("botao2");
+    const button3 = document.getElementById("botao3");
+    
+
+    button0.classList.remove(styles.botaoCrewClick);
+    button0.classList.add(styles.botaoCrew);
+
+    button1.classList.remove(styles.botaoCrewClick);
+    button1.classList.add(styles.botaoCrew);
+
+    button2.classList.remove(styles.botaoCrewClick);
+    button2.classList.add(styles.botaoCrew);
+
+    button3.classList.remove(styles.botaoCrew);
+    button3.classList.add(styles.botaoCrewClick);
+
+    setSelectedCrew({
+        image: AnoushehAnsari,
+        title: "FLIGHT ENGINEER",
+        name: "ANOUSHEH ANSARI",
+        text: (
+            <>
+                Anousheh Ansari is an Iranian American engineer and<br/>
+                co-founder of Prodea Systems. Ansari was the fourth<br/>
+                self-funded space tourist, the first self-funded woman to<br/>
+                fly to the ISS, and the first Iranian in space.<br/>
+                <span>&nbsp;</span><br />
+            </>
+        ),
+    });
+  };
+
+
+
+
+
+
+  const handleBackToInitialClick = () => {
+
+    const button0 = document.getElementById("botao0");
+    const button1 = document.getElementById("botao1");
+    const button2 = document.getElementById("botao2");
+
+    button0.classList.remove(styles.botaoCrew);
+    button0.classList.add(styles.botaoCrewClick);
+
+    button1.classList.remove(styles.botaoCrewClick);
+    button1.classList.add(styles.botaoCrew);
+
+    button2.classList.remove(styles.botaoCrewClick);
+    button2.classList.add(styles.botaoCrew);
+
+
     setSelectedCrew(initialCrewState);
+
+
   };
 
   return (
@@ -58,10 +177,10 @@ export default function Crew() {
             <div></div>
           </div>
           <div className={styles.conjuntoBotao}>
-            <button className={styles.botaoCrewClick} id="botao0" onClick={handleBackToDouglasClick}></button>
-            <button className={styles.botaoCrew} id="botao1" onClick={handleButtonClick}></button>
-            <button className={styles.botaoCrew} id="botao2"></button>
-            <button className={styles.botaoCrew} id="botao3"></button>
+            <button className={styles.botaoCrewClick} id="botao0" onClick={handleBackToInitialClick}></button>
+            <button className={styles.botaoCrew} id="botao1" onClick={handleButtonClick1}></button>
+            <button className={styles.botaoCrew} id="botao2" onClick={handleButtonClick2}></button>
+            <button className={styles.botaoCrew} id="botao3" onClick={handleButtonClick3}></button>
           </div>
         </div>
         <div className={styles.espacoCrew}></div>
--- src/components/Destination.js
+++ src/components/Destination.js
@@ -1,72 +1,313 @@
 import React, { Component } from "react";
-import Moon from '../img/image-moon.png';
+import Mars from '../img/image-mars.png';
 import styles from "../components/modules/Destination.module.css";
+import InitialDestinationImage from '../img/image-moon.png';
+import Europa from '../img/image-europa.png'
 
 export default class Destination extends Component {
-  
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {
-  //     activeSection: "Destination",
-  //   };
-  // }
-  // o construtor não faz diferença?!
-  render(){
+  constructor(props) {
+    super(props);
 
-  
+    this.state = {
+      visibility: {
+        e: "visible",
+        f: "hidden",
+        g: "hidden",
+        h: "hidden",
+      },
+      animation: {
+        opacity: 1,
+        animation: "",
+      },
+      selectedDestination: {
+        image: InitialDestinationImage,
+        destination: "MOON",
+        travel: '3 DAYS',
+        dist: '384,400 KM',
+        text1: (
+          <>
+            See our planet as you've never seen it before. A perfect
+          </>
+        ),
+        text2: (
+          <>
+            relaxing trip away to help regain perspective and come
+          </>
+        ),
+        text3: (
+          <>
+            back refreshed. While you're there, take in some history
+          </>
+        ),
+        text4: (
+          <>
+            by visiting the Luna 2 and Apollo 11 landing sites.
+          </>
+        )
+      },
+    };
+    
+  }
+
+  handleBackToInitialClick = () => {
+    this.setState({
+      visibility: {
+        e: "visible",
+        f: "hidden",
+        g: "hidden",
+        h: "hidden",
+      },
+      animation: {
+        opacity: 1,
+        animation: "fadeIn 2s linear",
+      },
+      selectedDestination: {
+        image: InitialDestinationImage,
+        destination: "MOON",
+        travel: '3 DAYS',
+        dist: '384,400 KM',
+        text1: (
+          <>
+            See our planet as you've never seen it before. A perfect
+          </>
+        ),
+        text2: (
+          <>
+            relaxing trip away to help regain perspective and come
+          </>
+        ),
+        text3: (
+          <>
+            back refreshed. While you're there, take in some history
+          </>
+        ),
+        text4: (
+          <>
+            by visiting the Luna 2 and Apollo 11 landing sites.
+          </>
+        )
+      }
+    });
+    this.insertAnimationKeyframes("fadeIn");
+  };
+
+  handleButtonClick1 = () => {
+    const { visibility } = this.state;
+
+    this.setState({
+      visibility: {
+        e: "hidden",
+        f: "visible",
+        g: "hidden",
+        h: "hidden",
+      },
+      animation: {
+        opacity: 1,
+        animation: "fadeIn 2s linear",
+      },
+      selectedDestination: {
+        image: Mars,
+        destination: "MARS",
+        travel: '9 MONTHS',
+        dist: '225 MIL. KM',
+        text1: (
+          <>
+            Don't forget to pack your hiking boots. You'll need them to
+          </>
+        ),
+        text2: (
+          <>
+            tackle Olympus Mons, the tallest planetary mountain in
+          </>
+        ),
+        text3: (
+          <>
+            our solar system. It's two and a half times the size of
+          </>
+        ),
+        text4: (
+          <>
+            Everest!
+          </>
+        ),
+      },
+    });
+
+    this.insertAnimationKeyframes("fadeIn");
+  };
+
+
+  handleButtonClick2 = () => {
+    const { visibility } = this.state;
+
+    this.setState({
+      visibility: {
+        e: "hidden",
+        f: "hidden",
+        g: "visible",
+        h: "hidden",
+      },
+      animation: {
+        opacity: 1,
+        animation: "fadeIn 2s linear",
+      },
+      selectedDestination: {
+        image: Europa,
+        destination: "EUROPA",
+        travel: '3 YEARS',
+        dist: '628 MIL. KM',
+        text1: (
+          <>
+            The smallest of the four Galilean moons orbiting Jupiter,
+          </>
+        ),
+        text2: (
+          <>
+            Europa is a winter lover's dream. With an icy surface, it's
+          </>
+        ),
+        text3: (
+          <>
+            perfect for a bit of ice skating, curling, hockey, or simple
+          </>
+        ),
+        text4: (
+          <>
+            relaxation in your snug wintery cabin.
+          </>
+        ),
+      },
+    });
+
+    this.insertAnimationKeyframes("fadeIn");
+  };
+
+  handleButtonClick3 = () => {
+    const { visibility } = this.state;
+
+    this.setState({
+      visibility: {
+        e: "hidden",
+        f: "hidden",
+        g: "hidden",
+        h: "visible",
+      },
+      animation: {
+        opacity: 1,
+        animation: "fadeIn 2s linear",
+      },
+      selectedDestination: {
+        image: Europa,
+        destination: "TITAN",
+        travel: '7 YEARS',
+        dist: '1.6 BIL. KM',
+        text1: (
+          <>
+            The only moon known to have a dense atmosphere other
+          </>
+        ),
+        text2: (
+          <>
+            than Earth, Titan is a home away from home (just a few
+          </>
+        ),
+        text3: (
+          <>
+            hundred degrees colder!). As a bonus, you get striking
+          </>
+        ),
+        text4: (
+          <>
+            views of the Rings of Saturn.
+          </>
+        ),
+      },
+    });
+
+    this.insertAnimationKeyframes("fadeIn");
+  };
+
+
+  insertAnimationKeyframes = (keyframeName) => { // tentar entender esse metodo aqui.
+    const { animation } = this.state;
+    const animationCount = animation.animation ? animation.animationCount + 1 : 1;
+    const dynamicKeyframeName = `${keyframeName}${animationCount}`;
+
+    this.setState({
+      animation: {
+        opacity: 1,
+        animation: `${dynamicKeyframeName} 2s linear`,
+        animationCount: animationCount,
+      },
+    }); 
+
+    const styleSheet = document.styleSheets[0];
+    styleSheet.insertRule(`
+      @keyframes ${dynamicKeyframeName} {
+        from {
+          opacity: 0;
+        }
+        to {
+          opacity: 1;
+        }
+      }
+    `, styleSheet.cssRules.length);
+  };
+
+  render() {
+    const { selectedDestination, visibility, animation } = this.state;
+
     return (
       <div className={styles.alinha}>
         <div className={styles.linha}>
           <div className={styles.tituloDest}>
-              <div className={styles.num}>01</div>
-              <div className={styles.letr}>PICK YOUR DESTINATION</div>
+            <div className={styles.num}>01</div>
+            <div className={styles.letr}>PICK YOUR DESTINATION</div>
           </div>
-          <img className={styles.imagem0} src={Moon} />
+          <img id="image" src={selectedDestination.image} className={styles.imagem0} style={{ opacity: animation.opacity, animation: animation.animation }} alt="Destination" />
         </div>
-        
-        
+
         <div className={styles.alin2}>
           <div className={styles.al}>
-            <div id={styles.barra4}>
+            <div id={styles.barra4} onClick={this.handleBackToInitialClick}>
               <div className={styles.letra0}>MOON</div>
-              <div id={styles.e} className={styles.subBarra0} style={{ visibility: "visible" }}></div>
+              <div id={styles.e} className={styles.subBarra0} style={{ visibility: visibility.e }}></div>
             </div>
-            <div id={styles.barra5}>
+            <div id={styles.barra5} onClick={this.handleButtonClick1}>
               <div className={styles.letra0}>MARS</div>
-              <div id={styles.f} className={styles.subBarra1}></div>
+              <div id={styles.f} className={styles.subBarra1} style={{ visibility: visibility.f }}></div>
             </div>
-            <div id={styles.barra6}>
+            <div id={styles.barra6} onClick={this.handleButtonClick2}>
               <div className={styles.letra0}>EUROPA</div>
-              <div id={styles.g} className={styles.subBarra2}></div>
+              <div id={styles.g} className={styles.subBarra2} style={{ visibility: visibility.g }}></div>
             </div>
-            <div id={styles.barra7}>
+            <div id={styles.barra7} onClick={this.handleButtonClick3}>
               <div className={styles.letra0}>TITAN</div>
-              <div id={styles.h} className={styles.subBarra3}></div>
+              <div id={styles.h} className={styles.subBarra3} style={{ visibility: visibility.h }}></div>
             </div>
           </div>
-        
-          <div className={styles.text0}>MOON</div>
-          
-          <div className={styles.text1}>See our planet as you've never seen it before. A perfect</div>
-          <div className={styles.text2}>relaxing trip away to help regain perspective and come</div>
-          <div className={styles.text3}>back refreshed. While you're there, take in some history</div>
-          <div className={styles.text4}>by visiting the Luna 2 and Apollo 11 landing sites.</div>
+
+          <div className={styles.text0} style={{ opacity: animation.opacity, animation: animation.animation }}>{selectedDestination.destination}</div>
+          <div className={styles.text1} style={{ opacity: animation.opacity, animation: animation.animation }}>{selectedDestination.text1}</div>
+          <div className={styles.text2} style={{ opacity: animation.opacity, animation: animation.animation }}>{selectedDestination.text2}</div>
+          <div className={styles.text3} style={{ opacity: animation.opacity, animation: animation.animation }}>{selectedDestination.text3}</div>
+          <div className={styles.text4} style={{ opacity: animation.opacity, animation: animation.animation }}>{selectedDestination.text4}</div>
+
           <hr className={styles.linhaPersonalizada} />
-        
+
           <div className={styles.alin3}>
             <div>
               <div className={styles.text5}>AVG. DISTANCE</div>
-              <div className={styles.numDist0}>384,400 KM</div>
+              <div className={styles.numDist0} style={{ opacity: animation.opacity, animation: animation.animation }}>{selectedDestination.dist}</div>
             </div>
             <div className={styles.espaco1}></div>
             <div>
               <div className={styles.text5}>EST. TRAVEL TIME</div>
-              <div className={styles.numDist0}>3 DAYS</div>
+              <div className={styles.numDist0} style={{ opacity: animation.opacity, animation: animation.animation }}>{selectedDestination.travel}</div>
             </div>
           </div>
         </div>
       </div>
     );
-
   }
 }
